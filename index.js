4c5209550e9bccd1d2f7d5bd893ad059b0032e8f
ABartolomeu-coder <124580703+ABartolomeu-coder@users.noreply.github.com>
2023-10-20 17:07:36 +0100

    Add win condition; Pacman chomp animation

--- index.js
+++ index.js
@@ -27,19 +27,37 @@ class Player {
     this.position = position;
     this.velocity = velocity;
     this.radius = 15;
+    this.radians = 0.75;
+    this.openRate = 0.12;
+    this.rotation = 0;
   }
 
   draw() {
+    c.save();
+    c.translate(this.position.x, this.position.y);
+    c.rotate(this.rotation);
+    c.translate(-this.position.x, -this.position.y);
     c.beginPath();
-    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.arc(
+      this.position.x,
+      this.position.y,
+      this.radius,
+      this.radians,
+      Math.PI * 2 - this.radians
+    );
+    c.lineTo(this.position.x, this.position.y);
     c.fillStyle = "yellow";
     c.fill();
     c.closePath();
+    c.restore();
   }
   update() {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
+    if (this.radians < 0 || this.radians > 0.75) this.openRate = -this.openRate;
+    this.radians += this.openRate;
   }
 }
 
@@ -478,6 +496,12 @@ function animate() {
     }
   }
 
+  //Win condition goes here
+  if (pellets.length === 0) {
+    console.log("you win");
+    cancelAnimationFrame(animationId);
+  }
+
   //power ups go
   for (let i = powerUps.length - 1; 0 <= i; i--) {
     const powerUp = powerUps[i];
@@ -610,6 +634,11 @@ function animate() {
       ghost.prevCollisions = [];
     }
   });
+
+  if (player.velocity.x > 0) player.rotation = 0;
+  else if (player.velocity.x < 0) player.rotation = Math.PI;
+  else if (player.velocity.y > 0) player.rotation = Math.PI / 2;
+  else if (player.velocity.y < 0) player.rotation = Math.PI * 1.5;
 }
 animate();
 
